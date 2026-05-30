49968f154f659e7d8c4937c22c3039a0d0df4c49
bobrkurvar <andrey.bogdanov2005@mail.ru>
2026-05-30 10:06:04 +0300

    пояснил работу в комментариях в main

--- src/main/java/somepackage/Main.java
+++ src/main/java/somepackage/Main.java
@@ -7,16 +7,29 @@ public class Main {
 
         SomeBean bean1 = injector.inject(new SomeBean());
         System.out.print("Результат bean1.foo(): ");
-        bean1.foo(); // Если в файле SomeImpl и SODoer, выведет: AC
+        /*
+         * Ожидаемый результат: AC
+         * Почему: В config.properties прописано:
+         * somepackage.SomeInterface=somepackage.SomeImpl (метод doSomething выводит "A")
+         * somepackage.SomeOtherInterface=somepackage.SODoer (метод doSomeOther выводит "C")
+         */
+        bean1.foo();
 
 
-        System.out.println("Динамическая смена реализации (Имитация изменения конфига)");
+        System.out.println("\nДинамическая смена реализации (Имитация изменения конфига)");
+        // Переопределяется настройка: теперь для SomeInterface будет использоваться OtherImpl
         injector.setProperty("somepackage.SomeInterface", "somepackage.OtherImpl");
 
         SomeBean bean2 = new SomeBean();
         injector.inject(bean2);
 
         System.out.print("Результат bean2.foo(): ");
+        /*
+         * Ожидаемый результат: BC
+         * Почему: динамически подменяется класс для SomeInterface на OtherImpl.
+         * Метод doSomething() у OtherImpl выводит "B".
+         * Привязка для SomeOtherInterface не менялась, поэтому там остался SODoer ("C").
+         */
         bean2.foo();
     }
 }
